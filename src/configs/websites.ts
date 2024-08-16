7b87677a982f031ec0777da2c6a665c6d48eb81d
Akash Sharma <aakash6263264@gmail.com>
2024-08-16 19:30:03 +0530

    fixed safari coding profile url

--- src/configs/websites.ts
+++ src/configs/websites.ts
@@ -5,6 +5,12 @@ const websites: WebsitesData = {
     title: "SNS Links",
     sites: [
       {
+        id: "my-email",
+        title: "Email",
+        img: "img/sites/gmail.svg",
+        link: "mailto:aakash6263264@gmail.com",
+      },
+      {
         id: "my-github",
         title: "Github",
         img: "img/sites/github.svg",
@@ -14,19 +20,25 @@ const websites: WebsitesData = {
         id: "my-linkedin",
         title: "Linkedin",
         img: "img/sites/linkedin.svg",
-        link: "www.linkedin.com/in/aakashsharma003",
+        link: "https://www.linkedin.com/in/akash-sharma-aab4a0227/",
       },
       {
         id: "my-twitter",
         title: "Twitter",
         img: "img/sites/twitter.svg",
-        link: "https://www.twitter.com/aakashsharma03",
+        link: "https://www.twitter.com/akashsharma_03",
       },
       {
-        id: "my-email",
-        title: "Email",
-        img: "img/sites/gmail.svg",
-        link: "mailto:aakash6263264@gmail.com",
+        id: "leetcode",
+        title: "LeetCode",
+        img: "img/sites/leetcode.svg",
+        link: "https://leetcode.com/u/aakashsharma_03/",
+      },
+      {
+        id: "GeeksforGeeks",
+        title: "GeeksforGeeks",
+        img: "img/sites/gfg.png",
+        link: "https://www.geeksforgeeks.org/user/demaxxer/",
       },
     ],
   },
@@ -34,10 +46,16 @@ const websites: WebsitesData = {
     title: "Frequently Visited",
     sites: [
       {
+        id: "hacker-news",
+        title: "Hacker News",
+        img: "img/sites/hacker.svg",
+        link: "https://news.ycombinator.com/",
+      },
+      {
         id: "github",
         title: "Github",
         img: "img/sites/github.svg",
-        link: "https://github.com/aakashsharma003",
+        link: "https://github.com/",
       },
       {
         id: "twitter",
@@ -52,17 +70,11 @@ const websites: WebsitesData = {
         link: "https://leetcode.com/",
       },
       {
-        id: "reddit",
-        title: "Reddit",
-        img: "img/sites/reddit.svg",
-        link: "https://www.reddit.com/",
+        id: "GeeksforGeeks",
+        title: "GeeksforGeeks",
+        img: "img/sites/gfg.png",
+        link: "https://www.geeksforgeeks.org/",
       },
-      {
-        id: "hacker-news",
-        title: "Hacker News",
-        img: "img/sites/hacker.svg",
-        link: "https://news.ycombinator.com/",
-      }
     ],
   },
 };
